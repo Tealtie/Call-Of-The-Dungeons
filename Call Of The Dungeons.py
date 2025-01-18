af10e677af458ebd112549e31e4a30e07d5e7ab5
Aqib <aqibdeen51211@gmail.com>
2025-01-18 20:57:21 +0000

    added spawn enemy

--- Call Of The Dungeons.py
+++ Call Of The Dungeons.py
@@ -1,5 +1,11 @@
+import random
+
 def init():
     print("\nWelcome To Call Of The Dungeons!")
-    print("ver: 0.0.0.1\n")
+    print("ver: 0.0.0.2\n")
+
+def spawnBasicEnemy():
+    global enemyHealth
+    enemyHealth = random.randint(5,20)
 
 init()
