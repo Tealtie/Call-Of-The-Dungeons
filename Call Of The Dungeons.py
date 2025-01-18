60347bc1f875a91280a8e1d7737ddf3e08c50c9c
Aqib <aqibdeen51211@gmail.com>
2025-01-18 21:17:33 +0000

    added input no functionallity

--- Call Of The Dungeons.py
+++ Call Of The Dungeons.py
@@ -3,14 +3,24 @@ import random
 def init():
     print("\nWelcome To Call Of The Dungeons!")
     print("ver: 0.0.0.2\n")
+    playerInit()
+    spawnBasicEnemy()
 
 def playerInit():
     global playerHealth
     playerHealth = 20
     
-
 def spawnBasicEnemy():
     global enemyHealth
     enemyHealth = random.randint(5,20)
 
+def fight():
+    global enemyHealth
+    enemyHealth -= random.randint(5,7)
+
+def playerInput():
+    print(f"An Enemy Has Spawned With {enemyHealth} HP While You Have {playerHealth} HP")
+    choice = input("What Do You Wish To Do?\n")
+
 init()
+playerInput()
