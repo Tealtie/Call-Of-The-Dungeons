f76e248bb0b7ef4163423e2c19b06745fcb16fdc
Aqib <aqibdeen51211@gmail.com>
2025-01-18 21:40:27 +0000

    major progress

--- Call Of The Dungeons.py
+++ Call Of The Dungeons.py
@@ -1,26 +1,44 @@
 import random
 
 def init():
-    print("\nWelcome To Call Of The Dungeons!")
-    print("ver: 0.0.0.2\n")
-    playerInit()
-    spawnBasicEnemy()
+	print("\nWelcome To Call Of The Dungeons!")
+	print("ver: 0.0.0.31\n")
+	playerInit()
+	spawnBasicEnemy()
+	global main
+	main = True
 
 def playerInit():
-    global playerHealth
-    playerHealth = 20
-    
+	global playerHealth
+	playerHealth = 20
+	
 def spawnBasicEnemy():
-    global enemyHealth
-    enemyHealth = random.randint(5,20)
+	global enemyHealth
+	enemyHealth = random.randint(5,20)
 
 def fight():
-    global enemyHealth
-    enemyHealth -= random.randint(5,7)
+	global enemyHealth
+	damage = random.randint(5,7)
+	enemyHealth -= damage
+	print(f"Well Done!")
+	print(f"You Dealt {damage} Damage!")
+	print(f"The Enemy Has {enemyHealth} HP Left!")
 
 def playerInput():
-    print(f"An Enemy Has Spawned With {enemyHealth} HP While You Have {playerHealth} HP")
-    choice = input("What Do You Wish To Do?\n")
+	print(f"An Enemy Has Spawned With {enemyHealth} HP While You Have {playerHealth} HP")
+	ask = True
+	while ask:
+		choice = input("What Do You Wish To Do?\n")
+		choice =  choice.lower()
+		if choice == "fight":
+			fight()
+			ask = False
+		else:
+			print('That Is Not A Valid Option!')
+			print('Try "Fight"')
 
 init()
-playerInput()
+
+class mainloop():
+	while main:
+		playerInput()
