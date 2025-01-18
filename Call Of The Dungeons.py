b490e15d53d2c60806a6535c7450ed677a86b0b9
Aqib <aqibdeen51211@gmail.com>
2025-01-18 21:01:24 +0000

    added playerInit

--- Call Of The Dungeons.py
+++ Call Of The Dungeons.py
@@ -4,6 +4,11 @@ def init():
     print("\nWelcome To Call Of The Dungeons!")
     print("ver: 0.0.0.2\n")
 
+def playerInit():
+    global playerHealth
+    playerHealth = 20
+    
+
 def spawnBasicEnemy():
     global enemyHealth
     enemyHealth = random.randint(5,20)
