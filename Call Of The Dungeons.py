259449fe96ff4244984bdaa33a5aa59a5251131b
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-21 20:40:13 +0000

    refactor game logic to improve readability and add command-line argument handling

--- Call Of The Dungeons.py
+++ Call Of The Dungeons.py
@@ -1,51 +1,57 @@
 import random
+import sys
 
 def init():
-	print("\nWelcome To Call Of The Dungeons!")
-	print("ver: 0.0.0.31\n")
-	playerInit()
-	spawnBasicEnemy()
-	global main
-	main = True
+    print("\nWelcome To Call Of The Dungeons!")
+    print("ver: 0.0.0.31\n")
+    playerInit()
+    spawnBasicEnemy()
+    global main
+    main = True
 
 def playerInit():
-	global playerHealth
-	playerHealth = 20
-	
+    global playerHealth
+    playerHealth = 20
+    
 def spawnBasicEnemy():
-	global enemyHealth
-	enemyHealth = random.randint(5,20)
+    global enemyHealth
+    enemyHealth = random.randint(5,20)
 
 def fight():
-	global enemyHealth
-	damage = random.randint(5,7)
-	enemyHealth -= damage
-	print(f"\nWell Done!")
-	print(f"You Dealt {damage} Damage!")
-	print(f"The Enemy Has {enemyHealth} HP Left!\n")
+    global enemyHealth
+    damage = random.randint(5,7)
+    enemyHealth -= damage
+    print(f"\nWell Done!")
+    print(f"You Dealt {damage} Damage!")
+    print(f"The Enemy Has {enemyHealth} HP Left!\n")
 
 def run():
-	print("You Ran Away!")
-	exit()
+    print("You Ran Away!")
+    exit()
 
 def playerInput():
-	print(f"An Enemy Has Spawned With {enemyHealth} HP While You Have {playerHealth} HP!")
-	ask = True
-	while ask:
-		choice = input("What Do You Wish To Do?\n")
-		choice =  choice.lower()
-		if choice == "fight":
-			fight()
-			ask = False
-		elif choice == "run":
-			run()
-			ask = False
-		else:
-			print('\nThat Is Not A Valid Option!')
-			print('Try "Fight or "Run"\n')
+    print(f"An Enemy Has Spawned With {enemyHealth} HP While You Have {playerHealth} HP!")
+    ask = True
+    while ask:
+        choice = input("What Do You Wish To Do?\n")
+        choice =  choice.lower()
+        if choice == "fight":
+            fight()
+            ask = False
+        elif choice == "run":
+            run()
+            ask = False
+        else:
+            print('\nThat Is Not A Valid Option!')
+            print('Try "Fight or "Run"\n')
 
 init()
 
 class mainloop():
-	while main:
-		playerInput()
+    while main:
+        if len(sys.argv) > 1:
+            choice = sys.argv[1]  # Get the first argument passed
+        else:
+            # Set a default choice if no argument is passed
+            exit()
+        playerInput()
